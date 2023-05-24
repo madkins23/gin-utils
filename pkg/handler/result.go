eee1d54b658be37e1724ef5cf163baabda35de0f
mAdkins <software@doorways.org>
2023-05-23 19:44:48 -0700

    Add JSONResult

--- pkg/handler/result.go
+++ pkg/handler/result.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 const (
 	hdrContentType             = "Content-Type"
-	hdrContentTypeValue        = "text/plain; charset=utf-8"
+	hdrContentTypeTextValue    = "text/plain; charset=utf-8"
+	hdrContentTypeJSONValue    = "application/json; charset=utf-8"
 	hdrContentTypeOptions      = "X-Content-Type-Options"
 	hdrContentTypeOptionsValue = "nosniff"
 )
@@ -16,11 +18,24 @@ const (
 // Substitute something specific to the containing application.
 func ErrorResult(writer http.ResponseWriter, code int, text ...string) {
 	// Note: copied from http.Error() source.
-	writer.Header().Set(hdrContentType, hdrContentTypeValue)
+	writer.Header().Set(hdrContentType, hdrContentTypeTextValue)
 	writer.Header().Set(hdrContentTypeOptions, hdrContentTypeOptionsValue)
 	writer.WriteHeader(code)
 	_, _ = fmt.Fprintln(writer, http.StatusText(code))
 	for _, t := range text {
 		_, _ = fmt.Fprintln(writer, t)
+	}
+}
+
+// JSONResult returns a JSON message representing the specified object
+func JSONResult(writer http.ResponseWriter, object any) {
+	if bytes, err := json.Marshal(object); err != nil {
+		ErrorResult(writer, http.StatusInternalServerError, err.Error())
+	} else {
+		writer.Header().Set(hdrContentType, hdrContentTypeJSONValue)
+		writer.WriteHeader(http.StatusOK)
+		if _, err = writer.Write(bytes); err != nil {
+			ErrorResult(writer, http.StatusInternalServerError, err.Error())
+		}
 	}
 }
--- pkg/handler/result_test.go
+++ pkg/handler/result_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,7 +22,7 @@ func TestErrorResult(t *testing.T) {
 	hdrType := header[hdrContentType]
 	assert.NotNil(t, hdrType)
 	assert.Len(t, hdrType, 1)
-	assert.Equal(t, hdrContentTypeValue, hdrType[0])
+	assert.Equal(t, hdrContentTypeTextValue, hdrType[0])
 	hdrOpts := header[hdrContentTypeOptions]
 	assert.NotNil(t, hdrOpts)
 	assert.Len(t, hdrOpts, 1)
@@ -29,4 +30,57 @@ func TestErrorResult(t *testing.T) {
 	body := rec.Body.String()
 	assert.Contains(t, body, "Bad")
 	assert.Contains(t, body, "doggy")
+}
+
+// Note: Use float64 instead of int because JSON marshal/unmarshal
+// will convert all numbers to float64 and it makes assert.Equal just work.
+
+type obj struct {
+	Text string
+	OK   bool
+}
+
+type thing struct {
+	Text   string
+	Number float64
+	List   []string
+	Dict   map[string]any
+	Sub    obj
+}
+
+func TestJSONResult(t *testing.T) {
+	rec := httptest.NewRecorder()
+	require.NotNil(t, rec)
+	anything := thing{
+		Text:   "something",
+		Number: 23,
+		List: []string{
+			"alpha",
+			"bravo",
+			"charile",
+		},
+		Dict: map[string]any{
+			"first":  float64(13),
+			"second": "hand",
+			"third":  false,
+		},
+		Sub: obj{
+			Text: "within",
+			OK:   true,
+		},
+	}
+	JSONResult(rec, anything)
+	assert.Equal(t, http.StatusOK, rec.Code)
+	result := rec.Result()
+	require.NotNil(t, result)
+	header := result.Header
+	require.NotNil(t, header)
+	hdrType := header[hdrContentType]
+	assert.NotNil(t, hdrType)
+	assert.Len(t, hdrType, 1)
+	assert.Equal(t, hdrContentTypeJSONValue, hdrType[0])
+	var received thing
+	body := rec.Body
+	require.NoError(t, json.Unmarshal(body.Bytes(), &received))
+	assert.Equal(t, anything, received)
 }
